9ca02b43ffe103925d5b1b958b2faa2f22b9c815
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-09 17:43:44 +0200

    Functions

--- FindUserNameByID.cs
+++ FindUserNameByID.cs
@@ -11,27 +11,25 @@ using System.Linq;
 
 namespace FunctionApp33
 {
-    public static class FindUserIDByName
+    public static class FindUserNameByID
     {
-        [FunctionName("FindUserIDByName")]
+        [FunctionName("FindUserNameByID")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "1/{Name}")] HttpRequest req, string Name)
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "{ID}")] HttpRequest req, int? ID)
         {
-            if (Name is null)
+            if (ID is  null)
             {
-
                 return new OkObjectResult("please add the name of the user you would like to find");
             }
 
             User user = new User();
             UserContext userContext = new UserContext();
-            var User = userContext.Users.Where(a => a.UserName == Name)?.FirstOrDefault();
+            var User = userContext.Users.Where(a => a.UserID == ID)?.FirstOrDefault();
             if (User is null)
             {
-
                 return new OkObjectResult("Could not find a user with that name");
             }
-            return new OkObjectResult($"the is for {Name} is {User.UserID}");
+            return new OkObjectResult($"the name for {ID} is {User.UserName}");
         }
     }
 }
